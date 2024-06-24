e30e590d6257a6cd936eedf94031779b675e5907
Harsh Piplodiya <harshpiplodiya29@gmail.com>
2024-06-25 03:53:14 +0530

    added register and login feature

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 
+// api endpoint, routes import
+import foodRouter from './routes/food.routes.js';
+import userRouter from './routes/user.route.js';
+
 // app config
 const app = express();
 const port = 4000;
@@ -15,11 +19,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-// api endpoint, routes import
-import userRouter from './routes/food.routes.js';
-
 // routes declaration 
-app.use("/api/v1/food", userRouter);
+app.use("/api/v1/food", foodRouter);
+app.use("/api/v1/user", userRouter);
 
 // ex: http://localhost:4000/api/v1/food
 
