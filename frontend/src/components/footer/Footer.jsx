d0a9d840c567403f267ac1c010e623e4cb7e2a8f
Harsh Piplodiya <harshpiplodiya29@gmail.com>
2024-06-30 12:10:54 +0530

    added contact links

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
                 <img src={assets.dineDash} alt="dineDash logo" />
                 <p>DineDash brings the best local restaurants straight to your door. From your favorite comfort foods to exotic flavors, we have it all. Our mission is to deliver deliciousness with convenience and a pinch of dash.</p>
                 <div className="footer-social-icons">
-                    <img src={assets.github_icon} alt="linkedin" />
-                    <img src={assets.linkedin_icon} alt="twitter" />
-                    <img src={assets.twitter_icon} alt="facebook" />
+                    <a href="https://github.com/Harsh-Piplodiya" ><img src={assets.github_icon} alt="github" /></a>
+                    <a href="https://www.linkedin.com/in/harsh-piplodiya-24266a234/"><img src={assets.linkedin_icon} alt="linkedin" /></a>
+                    <a href="https://twitter.com/harshp2910"><img src={assets.twitter_icon} alt="twitter" /></a>
                 </div>
             </div>
             <div className="footer-content-center">
