11e93d483e971bca9b7b56d18892a49bfb1a1537
Harsh Piplodiya <harshpiplodiya29@gmail.com>
2024-07-04 16:24:26 +0530

    debugging

--- admin/src/pages/add/Add.jsx
+++ admin/src/pages/add/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './add.css';
 import axios from 'axios';
 import { assets, url } from '../../assets/assets'
